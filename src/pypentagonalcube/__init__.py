4b7f8d8c69072135021cb1d860b50acbf557322f
Louis Cuthbert <louis.cuthbert.engineer@gmail.com>
2021-02-13 20:47:26 +0000

    :sparkles: Updating flask example

--- src/pypentagonalcube/__init__.py
+++ src/pypentagonalcube/__init__.py
@@ -2,3 +2,4 @@ from .log import logging
 from .requesting import (
     get_web_request_via_cache, url_response_is_cached
 )
+from .utils import read_from_environment
--- src/pypentagonalcube/templates/flask.py
+++ src/pypentagonalcube/templates/flask.py
@@ -1,7 +1,44 @@
-from flask import Flask, jsonify
+"""
+
+    a p i
+    API
+
+    A file containing the main definition for our Flask API.
+
+    :requirements:
+    pip install flask flask_cors
+
+"""
+#
+#   :code:
+
+#
+#   :packages:
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+#
+#   :flask:
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"message": "ok"}), 200
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "404, path not found", "path": request.path}), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({"error": f"{e}", "path": request.path}), 500
+
+
+#
+#   :blueprints:
+#   We're using Flask Blueprints to define things, we just need to register them with our application here.
+from package.api.blueprints.observations import blueprint_one
+active_blueprints = [
+    blueprint_one
+]
+for blueprint in active_blueprints:
+    app.register_blueprint(blueprint=blueprint)
